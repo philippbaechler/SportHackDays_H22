47842033ec591b15aeeadbd8f88e4bb6d1c75bec
Philipp Bächler <philipp.baechler@gmail.com>
2022-11-07 16:25:17 +0100

    Added worker bbox and ball velocity to the starter script.

--- start_workers.sh
+++ start_workers.sh
@@ -8,6 +8,10 @@ WORKER_1="player_aggregation.py"
 WORKER_1_ARGUMENTS="$WORKER_1 worker -l info --web-port=6066"
 WORKER_2="calculate_center_of_gravity.py"
 WORKER_2_ARGUMENTS="$WORKER_2 worker -l info --web-port=6067"
+WORKER_3="find_team_bbox.py"
+WORKER_3_ARGUMENTS="$WORKER_3 worker -l info --web-port=6068"
+WORKER_4="calculate_ball_v_and_dir.py"
+WORKER_4_ARGUMENTS="$WORKER_4 worker -l info --web-port=6069"
 
 
 if [[ $# -gt 0 ]]; then
@@ -19,11 +23,16 @@ if [ ! -e "$OUTPUT_PID_PATH/$OUTPUT_PID_FILE" ]; then
     echo "Starting all python workers!"
     "$PYTHON_EXE" ./$WORKER_1_ARGUMENTS & echo $! > "$OUTPUT_PID_PATH/$OUTPUT_PID_FILE"
     "$PYTHON_EXE" ./$WORKER_2_ARGUMENTS & echo $! >> "$OUTPUT_PID_PATH/$OUTPUT_PID_FILE"
+    "$PYTHON_EXE" ./$WORKER_3_ARGUMENTS & echo $! >> "$OUTPUT_PID_PATH/$OUTPUT_PID_FILE"
+    "$PYTHON_EXE" ./$WORKER_4_ARGUMENTS & echo $! >> "$OUTPUT_PID_PATH/$OUTPUT_PID_FILE"
+    
 else
     echo "Stopping all python workers!"
 
     kill $(pgrep -f $WORKER_1)
     kill $(pgrep -f $WORKER_2)
+    kill $(pgrep -f $WORKER_3)
+    kill $(pgrep -f $WORKER_4)
 
     rm "$OUTPUT_PID_PATH/$OUTPUT_PID_FILE"
 fi
