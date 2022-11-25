39cbf546eac7f27d84b3edc6fd46763d6668ea22
Philipp Baechler <philipp.baechler@gmail.com>
2022-11-25 17:50:24 +0100

    Start video later to match simulation data.

--- start_video_and_simulation.sh
+++ start_video_and_simulation.sh
@@ -1,5 +1,5 @@
 ssh -i .ssh/hslukey.sec ubuntu@86.119.35.55 './SportHackDays_H22/start_simulation.sh'
 
-vlc --zoom=0.5 --start-time=0 --stop-time=300 --play-and-exit ~/ownCloud/SHD_Sport_Hackdays/output.mp4
+vlc --zoom=0.5 --start-time=14 --stop-time=300 --play-and-exit ~/ownCloud/SHD_Sport_Hackdays/output.mp4
 
 ssh -i .ssh/hslukey.sec ubuntu@86.119.35.55 './SportHackDays_H22/stop_simulation.sh'
