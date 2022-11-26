018a4d883995018d029ec742617ab81a428d846b
Philipp Baechler <philipp.baechler@gmail.com>
2022-11-26 23:48:49 +0100

    Start time can be passed now through a parameter.

--- start_video_and_simulation.sh
+++ start_video_and_simulation.sh
@@ -1,4 +1,4 @@
-ssh -i .ssh/hslukey.sec ubuntu@86.119.35.55 './SportHackDays_H22/start_simulation.sh'
+ssh -i .ssh/hslukey.sec ubuntu@86.119.35.55 './SportHackDays_H22/start_simulation.sh "2022-08-21 10:35:43.595+0100"'
 
 vlc --zoom=0.5 --start-time=14 --stop-time=300 --play-and-exit ~/ownCloud/SHD_Sport_Hackdays/output.mp4
 
