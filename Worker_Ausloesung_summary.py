42b1395c7c38b5598437f969755e1052860f2cd1
Philipp Baechler <philipp.baechler@gmail.com>
2022-11-26 18:42:55 +0100

    Some bugfixes.

--- Worker_Ausloesung_summary.py
+++ Worker_Ausloesung_summary.py
@@ -2,13 +2,8 @@ import faust
 
 kafka_brokers = ['86.119.35.55:9092']
 
-app = faust.App(
-    "Auslösung-app",
-    broker="kafka_brokers",
-    topic_allow_declare=False,
-    topic_disable_leader=True,
-    consumer_auto_offset_reset="latest",
-)
+app = faust.App("Ausloesung-app", broker=kafka_brokers)
+
 #input topics
 centerOfGravityTopic = app.topic("centerOfGravity")
 filteredInputBallTopic = app.topic("filteredInputBallTopic")
@@ -23,17 +18,20 @@ COG_1 = 0
 async def helper_func(stream):
     global COG_0, COG_1
     async for streams in stream:
-        if streams['team']['team_0']['x'] >= 0:
-            COG_0 = 1
-        else:
-            COG_0 = 0
-        if streams['team']['team_1']['x'] > 0:
-            COG_1 = 1
-        else:
-            COG_1 = 0
+        if 'x' in streams['teams']['team_0']:
+            if streams['teams']['team_0']['x'] >= 0:
+                COG_0 = 1
+            else:
+                COG_0 = 0
+        if 'x' in streams['teams']['team_1']:
+            if streams['teams']['team_1']['x'] > 0:
+                COG_1 = 1
+            else:
+                COG_1 = 0
+
 
 @app.agent(filteredInputBallTopic)
-async def main_func(stream):
+async def process(stream):
     global COG_0, COG_1
     async for streams in stream:
         if -0.1 <= streams['x'] >= 0.1:
