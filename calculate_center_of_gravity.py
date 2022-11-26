6b30fc4e3c116d13492325f86ae6e42c103ad106
Philipp Baechler <philipp.baechler@gmail.com>
2022-11-26 15:32:35 +0100

    Excluded goal keeper from calculation of enclosure box and center of gravity.

--- calculate_center_of_gravity.py
+++ calculate_center_of_gravity.py
@@ -24,16 +24,28 @@ def calculate_center_of_gravity(team_player_pos):
     return sum(x_list)/len(x_list), sum(y_list)/len(y_list)
 
 
+def get_field_players(all_teams):
+    field_players_0 = all_teams["team_0"]
+    field_players_1 = all_teams["team_1"]
+    if "30" in field_players_0:
+        field_players_0.remove("30")
+    if "67" in field_players_1:
+        field_players_1.remove("67")
+    return field_players_0, field_players_1
+
+
 @app.agent(playerPositionGroupedTopic)
 async def process(positions):
     global new_msg
     async for position in positions:
-        if len(position["teams"]["team_0"]) >= 3:
-            x,y = calculate_center_of_gravity(position["teams"]["team_0"])
+        field_players_0, field_players_1 = get_field_players(position["teams"])
+
+        if len(field_players_0) >= 3:
+            x,y = calculate_center_of_gravity(field_players_0)
             new_msg["teams"]["team_0"]["x"] = x
             new_msg["teams"]["team_0"]["y"] = y
-        if len(position["teams"]["team_1"]) >= 3:
-            x,y = calculate_center_of_gravity(position["teams"]["team_1"])
+        if len(field_players_1) >= 3:
+            x,y = calculate_center_of_gravity(field_players_1)
             new_msg["teams"]["team_1"]["x"] = x
             new_msg["teams"]["team_1"]["y"] = y
         if len(new_msg["teams"]["team_0"]) > 0 or len(new_msg["teams"]["team_1"]) > 0:
