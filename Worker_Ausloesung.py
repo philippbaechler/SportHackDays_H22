2d0391b829bcc81a019508394fb53046dd2cd99f
Jana Setz <janamsetz@gmail.com>
2022-11-26 18:48:52 +0100

    test Worker Ausloesung

--- Worker_Ausloesung.py
+++ Worker_Ausloesung.py
@@ -2,7 +2,7 @@ import faust
 
 kafka_brokers = ['86.119.35.55:9092']
 
-app = faust.App('calculate-EB-app', broker=kafka_brokers)
+app = faust.App('Ausloesung1-app', broker=kafka_brokers)
 
 AusloesungSummaryTopic = app.topic("AusloesungSummary")
 AusloesungTopic = app.topic('AusloesungTopic')
@@ -11,15 +11,19 @@ AusloesungTopic = app.topic('AusloesungTopic')
 @app.agent(AusloesungSummaryTopic)
 async def process(summaries):
     async for summary in summaries:
-        if summary['team_0']['COG'] == 1 and summary['team_0']['Ball_possession'] == 1 and summary['team_0']['Ball_position'] == 1:
+        if summary['team_0']['COG'] == 1 and summary['team_0']['Ball_position'] == 1:
             auslösung_0 = 'YES'
         else:
             auslösung_0 = "NO"
-        if summary['team_1']['COG'] == 1 and summary['team_1']['Ball_possession'] == 1 and summary['team_1']['Ball_position'] == 1:
+        if summary['team_1']['COG'] == 1  and summary['team_1']['Ball_position'] == 1:
             auslösung_1 = 'YES'
         else:
             auslösung_1 = "NO"
-        await AusloesungTopic.send(value={'time': box['time'], 'team_0': {'Auslösung': auslösung_0},
+        await AusloesungTopic.send(value={'time': summary['time'], 'team_0': {'Auslösung': auslösung_0},
                                                 'team_1': {'Auslösung': auslösung_1}})
 
 app.main()
+
+
+#and summary['team_0']['Ball_possession'] == 1
+#and summary['team_1']['Ball_possession'] == 1
